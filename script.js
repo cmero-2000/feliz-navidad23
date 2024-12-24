4f3ff2268607e33668a5f23414e6acad69164ff0
cmero-2000 <mero2carlos29@gmail.com>
2024-12-24 00:38:00 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Función que muestra el mensaje al hacer clic en el regalo
+function showMessage() {
+    // Obtiene el div del mensaje
+    const messageDiv = document.getElementById("message");
+
+    // Cambia el estilo para mostrar el mensaje
+    messageDiv.style.display = "block";
+}
